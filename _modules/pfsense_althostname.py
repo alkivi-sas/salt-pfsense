7947b2c313b698ad4600c221d23c2d8becfb97ba
Alkivi <admin@alkivi.fr>
2019-09-04 17:14:08 +0200

    feat(althostname): manage althostname

---
+++ _modules/pfsense_althostname.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Module for running pfsense module via fauxapi
+"""
+from __future__ import absolute_import
+
+# Import Python libs
+import os
+import re
+import base64
+import hashlib
+import binascii
+import logging
+
+# Import Salt libs
+import salt.utils.files
+import salt.utils.stringutils
+import pfsense
+from salt.exceptions import (
+    CommandExecutionError,
+    SaltInvocationError,
+)
+
+# Import 3rd-party libs
+from salt.ext import six
+from salt.ext.six.moves import range
+
+logger = logging.getLogger(__name__)
+
+def __virtual__():
+    if os.path.isfile('/etc/pf.os'):
+        return True
+    else:
+        return False
+
+def _get_client():
+    return pfsense.FauxapiLib(debug=True)
+
+
+def list_hostnames():
+    '''
+    Return the alternatives hostnames of the webgui
+    CLI Example:
+    .. code-block:: bash
+        salt '*' pfsense_althostnames.list_hostnames
+    '''
+    client = _get_client()
+    config = client.config_get()
+
+    return config['system']['webgui']['althostnames'].split(' ')
+
+
+def has_hostname(hostname):
+    '''
+    Return true if the hostname is present in althostnames
+    CLI Example:
+    .. code-block:: bash
+        salt '*' pfsense_althostnames.has_hostname test
+    '''
+    if hostname== '':
+        raise SaltInvocationError('alias can not be an empty string')
+
+    hostnames = list_hostnames()
+    if hostname in hostnames:
+        return True
+    else:
+        return False
+
+def add_hostname(hostname):
+    '''
+    Add the entry to the list
+    exist.
+    CLI Example:
+    .. code-block:: bash
+        salt '*' pfsense_aliases.set_target alias target
+    '''
+
+    if hostname == '':
+        raise SaltInvocationError('alias can not be an empty string')
+
+    current_hostnames = list_hostnames()
+    if hostname in current_hostnames:
+        return True
+
+    current_hostnames.append(hostname)
+
+    client = _get_client()
+    config = client.config_get()
+
+    config['system']['webgui']['althostnames'] = ' '.join(current_hostnames)
+    result = client.config_set(config)
+
+    if 'message' not in result:
+        raise CommandExecutionError('Problem when updating alias')
+    elif result['message'] != 'ok':
+        logger.warning(result)
+        raise CommandExecutionError('Problem when updating alias')
+    return True
+
+
+def rm_hostname(hostname):
+    '''
+    Remove an entry from the aliases file
+    CLI Example:
+    .. code-block:: bash
+        salt '*' pfsense_aliases.rm_alias alias
+    '''
+    current_hostnames = list_hostnames()
+    if hostname not in current_hostnames:
+        return True
+
+    client = _get_client()
+    config = client.config_get()
+
+    new_hostnames = []
+    for current_hostname in current_hostnames:
+        if current_hostname == hostname:
+            continue
+        new_hostnames.append(current_hostname)
+
+    config['system']['webgui']['althostnames'] = ' '.join(new_hostnames)
+    result = client.config_set(config)
+
+    if 'message' not in result:
+        raise CommandExecutionError('Problem when updating alias')
+    elif result['message'] != 'ok':
+        logger.warning(result)
+        raise CommandExecutionError('Problem when updating alias')
+    return True
---
+++ _states/pfsense_althostname.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Import python libs
+from __future__ import absolute_import, unicode_literals, print_function
+import re
+import sys
+
+# Import 3rd-party libs
+from salt.ext import six
+
+
+def present(name):
+    '''
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'result': True,
+           'comment': ''}
+
+    is_present = __salt__['pfsense_althostname.has_hostname'](name)
+    if __opts__['test']:
+        if not is_present:
+            ret['comment'] = 'Hostname is set to be created'
+            return ret
+        else:
+            ret['comment'] = 'Hostname is already present'
+        return ret
+
+    data = __salt__['pfsense_althostname.add_hostname'](name)
+
+    if is_present: 
+        ret['comment'] = ('Hostname {0} was already present'.format(name))
+    else:
+        ret['changes'][name] = 'New'
+        ret['comment'] = ('Hostname {0} added'.format(name))
+    return ret
+
+
+def absent(name):
+    ret = {'name': name,
+           'changes': {},
+           'result': True,
+           'comment': ''}
+
+    is_present = __salt__['pfsense_althostname.has_hostname'](name)
+    if __opts__['test']:
+        if not is_present:
+            ret['comment'] = 'Hostname is not present'
+        else:
+            ret['comment'] = 'Hostname set to be removed'
+        return ret
+
+    data = __salt__['pfsense_althostname.rm_hostname'](name)
+
+    if is_present: 
+        ret['changes'][name] = 'Removed'
+        ret['comment'] = ('Hostname {0} removed'.format(name))
+    else:
+        ret['comment'] = ('Hostname {0} already removed'.format(name))
+    return ret
